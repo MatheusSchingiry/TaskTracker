6e844afb1dffa325dc1d3b10599075b517203261
Matheus Schingiry <matheussschingiry@gmail.com>
2026-03-18 20:28:34 -0300

    feat(cli): add Main entrypoint

---
+++ tasktracker-cli/src/main/java/com/tasktracker/cli/Main.java
@@ -0,0 +1,9 @@
+package com.tasktracker.cli;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TaskTrackerCLI cli = new TaskTrackerCLI();
+        cli.start();
+    }
+}
